841276fdee6672e8d7f335ccf571b712e248257c
ZZ <ziqizhang.email@gmail.com>
2023-03-27 16:42:37 +0100

    web site name extract

--- python/src/sandbox/domain_name_extractor.py
+++ python/src/sandbox/domain_name_extractor.py
@@ -1,24 +1,28 @@
 from urllib.parse import urlparse
 import pandas as pd
 import wordsegment as ws
+import numpy as np
 
 ##if you adapt this code make sure to call this method in your new code before using the method below
 ws.load()
+stopwords=("www","ww","http","https","ftp","co","uk","com","net","org","nl","gov","in","ca","ie","nz","au","za",
+           "qa","th","bh","sa","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+           "s","t","u","v","w","x","y","z","sg","ph")
 
 def extract_domain_name(str):
     try:
-        if(len(str))==0:
-            return 0
+        if(len(str))==0 or str is np.nan:
+            return ""
         str="http://"+str
         obj=urlparse(str)
         hostname=obj.hostname
-        if hostname.startswith("www."):
-            hostname=hostname[4:]
-        if "." in hostname:
-            hostname=hostname[:hostname.index(".")]
-        if len(hostname)>0:
-            words=ws.segment(hostname)
-        return hostname+" "+ " ".join(words)
+        words=set(hostname.split("."))
+        words=words.difference(stopwords)
+        words=" ".join(words)
+        words=set(ws.segment(words))
+        words = words.difference(stopwords)
+
+        return " ".join(words)
 
     except:
         print("Cannot parse {}".format(str))
@@ -26,7 +30,7 @@ def extract_domain_name(str):
 
 if __name__ == "__main__":
     col_domain=6
-    in_file="/home/zz/Data/wdc_data_index/wdctable_202012_index_top100_export_forML/Place.csv"
+    in_file="/home/zz/Data/wdc_data_index/wdctable_202012_index_top100_export_forML/LocalBusiness.csv"
     df = pd.read_csv(in_file, header=0, delimiter=',', quoting=0, encoding="utf-8")
     domains=pd.unique(df['page_domain'])
     for d in domains:
